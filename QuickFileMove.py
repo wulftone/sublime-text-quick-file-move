e326d0be8b4419008352e4a958f07e056cccc328
Trevor Bortins <enabfluw@gmail.com>
2012-03-04 13:39:33 -0800

    first

---
+++ QuickFileMove.py
@@ -0,0 +1,52 @@
+# Moves or simply renames files via a keyboard shortcut using python os and shutil modules.
+# Configure the shortcut in the .sublime-keymap file of your choice.
+# See keymap file included in this project for a reference.
+# Currently, it only moves files to pre-existing directories.
+
+import os
+import shutil
+import sublime
+import sublime_plugin
+
+
+class QuickFileMoveCommand(sublime_plugin.WindowCommand):
+
+    def run(self):
+        view = self.window.active_view()
+        filename = view.file_name()
+        self.window.show_input_panel("Rename:", filename, lambda user_input: self.rename(view, filename, user_input), None, None)
+
+    def rename(self, view, old_filename, new_filename):
+        if not self.validateFileName(view, old_filename, new_filename):
+            return
+        if view.is_dirty():
+            view.window().run_command("save")
+        window = view.window()
+        self.fileOperations(window, old_filename, new_filename)
+        self.setSelection(view, window.active_view())
+
+    def validateFileName(self, view, old_file, new_file):
+        if len(new_file) is 0:
+            sublime.error_message("Error: No new filename given.")
+            return False
+        if view.is_loading():
+            sublime.error_message("Error: The file is still loading.")
+            return False
+        if view.is_read_only():
+            sublime.error_message("Error: The file is read-only.")
+            return False
+        if(new_file == old_file):
+            sublime.error_message("Error: The new file name was the same as the old one.")
+            return False
+        return True
+
+    def fileOperations(self, window, old_file, new_file):
+        window.run_command("close")
+        shutil.move(old_file, new_file)
+        window.open_file(new_file)
+        if old_file.endswith(".py"):
+            os.remove(old_file + "c")
+
+    def setSelection(self, old_view, new_view):
+        new_view.sel().clear()
+        new_view.sel().add_all(old_view.sel())
---
+++ README.md
@@ -0,0 +1,19 @@
+#Sublime Text 2 Quick File Renamer
+
+A simple ST2 plugin to help you rename your files
+
+###Usage
+
+Press `ctrl+shift+r` then enter your new filename!
+
+###Configuring
+
+Add `{ "keys": ["ctrl+shift+r"], "command": "quick_rename" }` to your user keybindings file and change
+the keymap to whatever you prefer.
+
+###Code
+
+The code is available on [GitHub](https://github.com/wulftone/sublime-text-2-quick-file-renamer)!
+
+Credit goes to [a.stenlund@gmail.com]() for making the
+[first version](http://sublime-text-community-packages.googlecode.com/svn/trunk/QuickRename/) for ST1.
